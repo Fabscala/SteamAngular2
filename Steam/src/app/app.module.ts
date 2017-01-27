b68c165b954391571dd2a1307126cda30fb9f198
Unknown <guilllaumeaxel@gmail.com>
2017-01-27 10:57:48 +0100

    On sait pas ce qu'on fait

--- Steam/src/app/app.module.ts
+++ Steam/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { TestService } from './test.service';
+
 import { AppComponent } from './app.component';
+import { TableauComponent } from './tableau/tableau.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableauComponent
   ],
   imports: [
     BrowserModule,
